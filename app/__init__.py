4c6765dc2b3e59f330feb0e0d3473c36d8bbdb93
Rob Harriman <rmharriman@gmail.com>
2017-02-22 21:57:06 -0500

    Working can js view

--- app/__init__.py
+++ app/__init__.py
@@ -20,6 +20,9 @@ def create_app(config_name):
     db.init_app(app)
 
     # Attach routes and custom error pages
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
     from .api import api as api_blueprint
     app.register_blueprint(api_blueprint, url_prefix="/api")
 
